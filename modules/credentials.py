41412b9b9e57c1bda38a0c8fd192dc3ea8b6031d
viperadnan-dummy <60770759+viperadnan-dummy@users.noreply.github.com>
2020-04-05 15:06:01 +0530

    Update and rename credentials.py.example to credentials.py

---
+++ modules/credentials.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Atul-Kadian
+
+class Creds(object):
+        TOKEN = "1115582385:AAFMhOZgoutv89jrVZ9VIsAVVfKc0jSHPRY"
+        CLIENT_ID = "858074304677-gfqd4f71uj2jjjl9tkfgva6c4rcavtki.apps.googleusercontent.com"
+        CLIENT_SECRET = "7VCZFIPpJQ6kgviH0rr73o3q"
+
